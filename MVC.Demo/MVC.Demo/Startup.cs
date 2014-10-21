2382bffd9ff643152869b8d674c741fa3aebc701
Wesley Cabus <wesley@gotsharp.be>
2014-10-21 08:55:59 +0200

    First commit

---
+++ MVC.Demo/MVC.Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC.Demo.Startup))]
+namespace MVC.Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
